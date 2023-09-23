913cad5045c60833dd08ba24747454a91b1b55c8
Prabhat Kumar <108947576+PrabhatKumardhiman@users.noreply.github.com>
2023-09-23 18:07:14 +0000

    Front-End Prep

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useState } from 'react';
 import './App.css';
+import MyForm from './components/MyForm';
+import Navbar from './components/Navbar';
+import Table from './components/Table';
 
 function App() {
+
+  const [value, setValue] = useState({userid : "", name : "", bankcode : ""})
+
   return (
     <div className="App">
-      
+      <header><Navbar /></header>
+      <main>
+        <div className="container mt-4">
+          <div className="row">
+            <div className="col-12 col-md-4">
+              <MyForm setValue = {setValue} value = {value} />
+            </div>
+            <div className="col-12 col-md-8">
+             <Table/>
+            </div>
+          </div>
+        </div>
+      </main>
+
     </div>
   );
 }
---
+++ src/components/MyForm.js
@@ -0,0 +1,121 @@
+import React, { useState } from 'react'
+
+const MyForm = (props) => {
+    // Destructuring Props
+    const { userid, name, bankcode } = props.value
+
+    // States
+    const [weatherData, setWeatherData] = useState([])
+    const [bankDetails, setbankDetails] = useState([])
+    console.log(weatherData);
+    console.log(bankDetails);
+
+    // On Change Function on Inputs 
+    const onChange = (e) => {
+        e.preventDefault();
+        // setting values of input tags via name property
+        props.setValue({ ...props.value, [e.target.name]: e.target.value })
+    }
+
+    // Handle submit function handeling click on add btn
+    const handlesubmit = async (e) => {
+        e.preventDefault();
+
+    }
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+        await fetchBankDetails()
+    }
+
+    //  Fetching Bank Details form razorpay API
+    const fetchBankDetails = async () => {
+        try {
+            const response = await fetch(`https://ifsc.razorpay.com/${bankcode}`);
+            const json = await response.json();
+            if (json === "Not Found") {
+                setWeatherData([])
+            } else {
+                setbankDetails(json)
+            }
+
+            await fetchWeatherDetails(json.DISTRICT)
+        } catch (error) {
+            alert("Unable To Fetch Weather Information")
+        }
+    }
+
+
+    // fetching weather information form weather API
+    const fetchWeatherDetails = async (city) => {
+        try {
+            const resp = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fc9e2431d2c395aa59fbad92138c8958`);
+            const json = await resp.json();
+            if (json.code === "404") {
+                setWeatherData([])
+                alert("Weather Data for city not found")
+            } else {
+                setWeatherData(json)
+            }
+        } catch (error) {
+
+        }
+    }
+
+
+    return (
+        <div>
+            <form onSubmit={handlesubmit}>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">
+                        Enter User Id
+                    </label>
+                    <input
+                        type="number"
+                        className="form-control"
+                        id="userid"
+                        aria-describedby="emailHelp"
+                        value={userid}
+                        name='userid'
+                        onChange={onChange}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">
+                        Name
+                    </label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="name"
+                        value={name}
+                        name='name'
+                        onChange={onChange}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">
+                        Bank IFSC Code
+                    </label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="bankcode"
+                        value={bankcode}
+                        name='bankcode'
+                        onChange={onChange}
+                    />
+                </div>
+                <button type="submit" onClick={handleClick} className="btn btn-primary me-4">
+                    Verify
+                </button>
+                <button type="submit" className="btn btn-primary">
+                    Add
+                </button>
+            </form>
+
+        </div>
+    )
+}
+
+export default MyForm
